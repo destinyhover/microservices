3d6ff49a8dda923ff2faa1c6006fb6c63b00b9bf
destinyhover <nikitos.21333@gmail.com>
2025-09-19 15:54:42 +0300

    Changes to be committed: 	modified:   go.mod 	new file:   go.sum 	modified:   order/internal/application/core/api/api.go

--- order/internal/application/core/api/api.go
+++ order/internal/application/core/api/api.go
@@ -1,1 +1,51 @@
 package api
+
+import (
+	"context"
+
+	"github.com/destinyhover/microservices/order/internal/application/core/domain"
+	"github.com/destinyhover/microservices/order/internal/ports"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type Application struct {
+	db      ports.DBPort
+	payment ports.PaymentPort
+}
+
+func NewApplication(db ports.DBPort, payment ports.PaymentPort) *Application {
+	return &Application{
+		db:      db,
+		payment: payment,
+	}
+}
+
+func (a Application) PlaceOrder(ctx context.Context, order domain.Order) (domain.Order, error) {
+	err := a.db.Save(ctx, &order)
+	if err != nil {
+		return domain.Order{}, err
+	}
+	paymentErr := a.payment.Charge(ctx, &order)
+	if paymentErr != nil {
+		st, _ := status.FromError(paymentErr)
+		fieldErr := &errdetails.BadRequest_FieldViolation{
+			Field:       "payment",
+			Description: st.Message(),
+		}
+		badReq := &errdetails.BadRequest{}
+		badReq.FieldViolations = append(badReq.FieldViolations, fieldErr)
+		orderStatus := status.New(codes.InvalidArgument, "prder creation failde")
+		statusWithDetails, _ := orderStatus.WithDetails(badReq)
+		return domain.Order{}, statusWithDetails.Err()
+	}
+	return order, nil
+}
+func (a Application) GetOrder(ctx context.Context, id int64) (domain.Order, error) {
+	order, err := a.db.Get(ctx, id)
+	if err != nil {
+		return domain.Order{}, err
+	}
+	return order, nil
+}
