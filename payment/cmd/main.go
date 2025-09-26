a9bf0af347b8243076b3b4992fc4c57dfdec7976
destinyhover <nikitos.21333@gmail.com>
2025-09-26 17:23:53 +0300

    modified:   payment/cmd/main.go 	modified:   payment/config/config.go

--- payment/cmd/main.go
+++ payment/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"os"
@@ -10,11 +11,30 @@ import (
 	"github.com/destinyhover/microservices/payment/internal/adapters/db"
 	gserver "github.com/destinyhover/microservices/payment/internal/adapters/grpc"
 	app "github.com/destinyhover/microservices/payment/internal/application/core/api"
+	"github.com/destinyhover/microservices/payment/internal/telemetry"
 )
 
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	slog.SetDefault(logger)
+	// Building up  tracing
+	ctx := context.Background()
+	shutdown, err := telemetry.SetupProvider(ctx, telemetry.Config{
+		ServiceName:    "payment",
+		ServiceVersion: "1.0.0",
+		Endpoint:       config.GetOLTPEndpoint(),
+		Insecure:       config.IsOTLPInsecure(),
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer func() {
+		if err := shutdown(context.Background()); err != nil {
+			slog.Error("failed to set up telemetry", "err", err)
+			os.Exit(1)
+		}
+	}()
 
 	dbAdapter, err := db.NewAdapter(config.GetDataSourceURL())
 	if err != nil {
--- payment/config/config.go
+++ payment/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"log/slog"
 	"os"
+	"strconv"
 )
 
 func GetVar(str string) string {
@@ -24,14 +25,36 @@ func GetPaymentPort() string {
 	return GetVar("PAYMENT_PORT")
 }
 
-// func GetPaymentSourceURL() string {
-// 	return GetVar("PAYMENT_SERVICE_URL")
-// }
-// func GetApplicationPort() int {
-// 	v := GetVar("APPLICATION_PORT")
-// 	i, err := strconv.Atoi(v)
-// 	if err != nil {
-// 		slog.Error("cannot conv", "port", v, "err", err)
-// 	}
-// 	return i
-// }
+//	func GetPaymentSourceURL() string {
+//		return GetVar("PAYMENT_SERVICE_URL")
+//	}
+//
+//	func GetApplicationPort() int {
+//		v := GetVar("APPLICATION_PORT")
+//		i, err := strconv.Atoi(v)
+//		if err != nil {
+//			slog.Error("cannot conv", "port", v, "err", err)
+//		}
+//		return i
+//	}
+//
+// Tracing variabales
+func GetOLTPEndpoint() string {
+	if endp, ok := os.LookupEnv("OTEL_EXPORTER_OTLP_ENDPOINT"); ok {
+		return endp
+	}
+	return ""
+}
+func IsOTLPInsecure() bool {
+	v := os.Getenv("OTEL_EXPORTER_OTLP_INSECURE")
+	if v == "" {
+		return true
+	}
+
+	ins, err := strconv.ParseBool(v)
+	if err != nil {
+		slog.Error("Cannot parse isotlpinsecure()", "err", err)
+		return false
+	}
+	return ins
+}
