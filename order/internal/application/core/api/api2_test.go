a74f736aa30ba51a69b2709060fa0e5588ae5ffb
destinyhover <nikitos.21333@gmail.com>
2025-10-06 14:10:47 +0300

    modified:   order/internal/application/core/api/api2_test.go

--- order/internal/application/core/api/api2_test.go
+++ order/internal/application/core/api/api2_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/destinyhover/microservices/order/internal/application/core/domain"
 	mocks "github.com/destinyhover/microservices/order/mocks/internal_/ports"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
@@ -13,6 +14,8 @@ import (
 func TestPlaceOrder_paymentFail(t *testing.T) {
 	payment := mocks.NewPaymentPort(t)
 	db := mocks.NewDBPort(t)
+	db.On("Save", mock.Anything, mock.AnythingOfType("*domain.Order")).Return(nil)
+
 	payment.On("Charge", mock.Anything, mock.AnythingOfType("*domain.Order")).Return(errors.New("insufficient balance"))
 	app := NewApplication(db, payment)
 
@@ -20,5 +23,26 @@ func TestPlaceOrder_paymentFail(t *testing.T) {
 		OrderItems: []domain.OrderItem{{ProductCode: "Camera", Quantity: 1, UnitPrice: 30}}})
 
 	require.Error(t, err)
-	db.AssertNotCalled(t, "Save", mock.Anything, mock.Anything)
+}
+func Test_Return_DB_Error(t *testing.T) {
+	payment := mocks.NewPaymentPort(t)
+	db := mocks.NewDBPort(t)
+	db.On("Save", mock.Anything, mock.Anything).Return(errors.New("db error"))
+	//payment.On("Charge", mock.Anything, mock.Anything).Return(nil)
+
+	application := NewApplication(db, payment)
+	_, err := application.PlaceOrder(t.Context(), domain.Order{
+		CustomerID: 1234,
+		OrderItems: []domain.OrderItem{
+			{
+				ProductCode: "Camera",
+				UnitPrice:   12.3,
+				Quantity:    3,
+			},
+		},
+		CreatedAt: 0,
+	})
+
+	assert.EqualError(t, err, "db error")
+	payment.AssertNotCalled(t, "Charge", mock.Anything, mock.Anything)
 }
